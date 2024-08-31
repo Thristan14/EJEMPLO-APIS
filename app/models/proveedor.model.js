19f09e4800694cac1973800ef6122ebcc7a51461
Thristan14 <thristanmenle@gmail.com>
2024-08-31 08:38:55 -0600

    actualización

--- app/models/proveedor.model.js
+++ app/models/proveedor.model.js
@@ -1,38 +1,40 @@
 module.exports = (sequelize, Sequelize) => {
-    const Book = sequelize.define('books', {
-      id: {
-        type: Sequelize.INTEGER,
-        autoIncrement: true,
-        primaryKey: true
-      },
-      bookcode: {
-        type: Sequelize.INTEGER
-      },
-      namebook: {
-        type: Sequelize.STRING
-      },
-      editorial: {
-        type: Sequelize.STRING
-      },
-      autor: {
-        type: Sequelize.STRING
-      },
-      genero: {
-        type: Sequelize.STRING
-      },
-      paisautor: {
-        type: Sequelize.STRING
-      },
-      numeropaginas: {
-        type: Sequelize.INTEGER
-      },
-      ano: {
-        type: Sequelize.DATE
-      },
-      preciolibro: {
-        type: Sequelize.DECIMAL(10, 2)
-      }
-    });
-  
-    return Book;
-  };
+  const Proveedor = sequelize.define('proveedores', {
+    id_proveedor: {
+      type: Sequelize.NUMERIC,
+      primaryKey: true
+    },
+    empresa: {
+      type: Sequelize.STRING(100)
+    },
+    direccion: {
+      type: Sequelize.STRING(100)
+    },
+    telefono: {
+      type: Sequelize.NUMERIC
+    },
+    nit: {
+      type: Sequelize.STRING(30)
+    },
+    ciudad: {
+      type: Sequelize.STRING(100)
+    },
+    pais: {
+      type: Sequelize.STRING(100)
+    },
+    contacto: {
+      type: Sequelize.STRING(100)
+    },
+    email: {
+      type: Sequelize.STRING(100)
+    },
+    telefono_contacto: {
+      type: Sequelize.NUMERIC
+    },
+    estatus: {
+      type: Sequelize.NUMERIC
+    }
+  });
+
+  return Proveedor;
+};
