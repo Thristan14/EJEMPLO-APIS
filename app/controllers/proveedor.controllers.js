a4a20865a39f696ea60a13cc777c877615afc21b
Thristan14 <thristanmenle@gmail.com>
2024-08-31 20:12:01 -0600

    Actualización

--- app/controllers/proveedor.controllers.js
+++ app/controllers/proveedor.controllers.js
@@ -1,101 +1,102 @@
 const db = require('../config/db.config.js');
-const Proveedor = db.Proveedor; // Asegúrate de que el nombre del modelo coincida con el definido en db.config.js
+const Proveedor = db.Proveedor; // Asegúrate de que el nombre del modelo coincida con el definido en tu archivo de Sequelize
 
 // Crear un nuevo proveedor
 exports.create = (req, res) => {
-    let proveedor = {};
+  const proveedor = {
+    id_proveedor: req.body.id_proveedor, // Opcional si `id_proveedor` es auto-incremental
+    empresa: req.body.empresa,
+    direccion: req.body.direccion,
+    telefono: req.body.telefono,
+    nit: req.body.nit,
+    ciudad: req.body.ciudad,
+    pais: req.body.pais,
+    contacto: req.body.contacto,
+    email: req.body.email,
+    telefono_contacto: req.body.telefono_contacto,
+    estatus: req.body.estatus
+  };
 
-    try {
-        proveedor.id_proveedor = req.body.id_proveedor;
-        proveedor.empresa = req.body.empresa;
-        proveedor.direccion = req.body.direccion;
-        proveedor.telefono = req.body.telefono;
-        proveedor.nit = req.body.nit;
-        proveedor.ciudad = req.body.ciudad;
-        proveedor.pais = req.body.pais;
-        proveedor.contacto = req.body.contacto;
-        proveedor.email = req.body.email;
-        proveedor.telefono_contacto = req.body.telefono_contacto;
-        proveedor.estatus = req.body.estatus;
-
-        Proveedor.create(proveedor).then(result => {
-            res.status(200).json({
-                message: "Proveedor creado exitosamente con id = " + result.id_proveedor,
-                proveedor: result,
-            });
-        });
-    } catch (error) {
-        res.status(500).json({
-            message: "¡Falló!",
-            error: error.message
-        });
-    }
-}
+  Proveedor.create(proveedor)
+    .then(result => {
+      res.status(200).json({
+        message: "Proveedor creado exitosamente con id = " + result.id_proveedor,
+        proveedor: result
+      });
+    })
+    .catch(error => {
+      res.status(500).json({
+        message: "¡Falló!",
+        error: error.message
+      });
+    });
+};
 
 // Recuperar todos los proveedores
 exports.findAll = (req, res) => {
-    Proveedor.findAll()
-        .then(proveedores => {
-            res.status(200).json({
-                message: "¡Información de todos los proveedores obtenida con éxito!",
-                proveedores: proveedores
-            });
-        })
-        .catch(error => {
-            res.status(500).json({
-                message: "¡Error!",
-                error: error
-            });
-        });
-}
+  Proveedor.findAll()
+    .then(proveedores => {
+      res.status(200).json({
+        message: "¡Información de todos los proveedores obtenida con éxito!",
+        proveedores: proveedores
+      });
+    })
+    .catch(error => {
+      res.status(500).json({
+        message: "¡Error!",
+        error: error.message
+      });
+    });
+};
 
 // Encontrar un proveedor por Id
 exports.findById = (req, res) => {
-    Proveedor.findByPk(req.params.id)
-        .then(proveedor => {
-            res.status(200).json({
-                message: "Proveedor recuperado exitosamente con id = " + req.params.id,
-                proveedor: proveedor
-            });
-        })
-        .catch(error => {
-            res.status(500).json({
-                message: "¡Error!",
-                error: error
-            });
-        });
-}
+  Proveedor.findByPk(req.params.id)
+    .then(proveedor => {
+      res.status(200).json({
+        message: "Proveedor recuperado exitosamente con id = " + req.params.id,
+        proveedor: proveedor
+      });
+    })
+    .catch(error => {
+      res.status(500).json({
+        message: "¡Error!",
+        error: error.message
+      });
+    });
+};
 
 // Actualizar un proveedor por Id
 exports.update = (req, res) => {
-    let id = req.params.id;
-    Proveedor.update(req.body, { where: { id_proveedor: id } })
-        .then(() => {
-            res.status(200).json({
-                message: "Proveedor actualizado exitosamente con id = " + id
-            });
-        })
-        .catch(error => {
-            res.status(500).json({
-                message: "Error -> No se pudo actualizar el proveedor con id = " + id,
-                error: error.message
-            });
-        });
-}
+  const id = req.params.id;
+  Proveedor.update(req.body, { where: { id_proveedor: id } })
+    .then(() => {
+      res.status(200).json({
+        message: "Proveedor actualizado exitosamente con id = " + id
+      });
+    })
+    .catch(error => {
+      res.status(500).json({
+        message: "Error -> No se pudo actualizar el proveedor con id = " + id,
+        error: error.message
+      });
+    });
+};
 
 // Eliminar un proveedor por Id
 exports.delete = (req, res) => {
-    let id = req.params.id;
-    Proveedor.destroy({ where: { id_proveedor: id } })
-        .then(() => {
-            res.status(200).json({
-                message: "Proveedor eliminado exitosamente con id = " + id
-            });
-        })
-        .catch(error => {
-            res.status(500).json({
-                message: "Error -> No se pudo eliminar el proveedor con id = " + id,
-                error: error.message
-            });
-        });
-}
+  const id = req.params.id;
+  Proveedor.destroy({ where: { id_proveedor: id } })
+    .then(() => {
+      res.status(200).json({
+        message: "Proveedor eliminado exitosamente con id = " + id
+      });
+    })
+    .catch(error => {
+      res.status(500).json({
+        message: "Error -> No se pudo eliminar el proveedor con id = " + id,
+        error: error.message
+      });
+    });
+};
+
--- app/models/proveedor.model.js
+++ app/models/proveedor.model.js
@@ -1,43 +1,49 @@
 module.exports = (sequelize, Sequelize) => {
-  const Proveedor = sequelize.define('proveedores', {
-    id_proveedor: {
-      type: Sequelize.NUMERIC,
-      primaryKey: true
-    },
-    empresa: {
-      type: Sequelize.STRING(100)
-    },
-    direccion: {
-      type: Sequelize.STRING(100)
-    },
-    telefono: {
-      type: Sequelize.NUMERIC
-    },
-    nit: {
-      type: Sequelize.STRING(30)
-    },
-    ciudad: {
-      type: Sequelize.STRING(100)
-    },
-    pais: {
-      type: Sequelize.STRING(100)
-    },
-    contacto: {
-      type: Sequelize.STRING(100)
-    },
-    email: {
-      type: Sequelize.STRING(100)
-    },
-    telefono_contacto: {
-      type: Sequelize.NUMERIC
-    },
-    estatus: {
-      type: Sequelize.NUMERIC
-    }
-  }, {
-    tableName: 'proveedores',  // Especifica el nombre de la tabla en la base de datos
-    timestamps: false         // Desactiva las marcas de tiempo (createdAt, updatedAt)
-  });
+	const Proveedor = sequelize.define('proveedor', {
+		id_proveedor: {
+			type: Sequelize.NUMERIC
+		},
+		empresa: {
+			type: Sequelize.STRING(100)
+		},
+		direccion: {
+			type: Sequelize.STRING(100)
+		},
+		telefono: {
+			type: Sequelize.NUMERIC
+		},
+		nit: {
+			type: Sequelize.STRING(30)
+		},
+		ciudad: {
+			type: Sequelize.STRING(100)
+		},
+		pais: {
+			type: Sequelize.STRING(100)
+		},
+		contacto: {
+			type: Sequelize.STRING(100)
+		},
+		email: {
+			type: Sequelize.STRING(100)
+		},
+		telefono_contacto: {
+			type: Sequelize.NUMERIC
+		},
+		estatus: {
+			type: Sequelize.NUMERIC
+		},
+		createdAt: {
+			type: Sequelize.DATE,
+			defaultValue: Sequelize.NOW,
+			allowNull: false
+		},
+		updatedAt: {
+			type: Sequelize.DATE,
+			defaultValue: Sequelize.NOW,
+			allowNull: false
+		}
+	});
 
-  return Proveedor;
-};
+	return Proveedor;
+}
